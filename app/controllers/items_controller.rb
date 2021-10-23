4dc877aa3e4af4f2f8b0a0d04c39886661e57991
Mmy26 <yu.b1994masamura@gmail.com>
2021-10-23 19:36:35 +0900

    MVCの設置とバリデーションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,5 +2,16 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
 
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :title, :explanation, :category_id, :status_id, :fee_id, :region_id, :shipped_day_id, :price)
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,12 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  
+  validates :title, presence: true
+  validates :explanation,    presence: true
+  validates :category_id,    presence: true
+  validates :status_id,      presence: true
+  validates :fee_id,         presence: true
+  validates :region_id,      presence: true
+  validates :shipped_day_id, presence: true
+  validates :price  ,        presence: true
+end
