997ae786122738c6c9c7d06aa01f5f67c749ac83
Mmy26 <yu.b1994masamura@gmail.com>
2021-10-24 18:39:23 +0900

    ActiveHashの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,4 +10,14 @@ class Item < ApplicationRecord
   validates :region_id,      presence: true
   validates :shipped_day_id, presence: true
   validates :price  ,        presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :region
+  
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 } 
+  validates :fee_id, numericality: { other_than: 1 } 
+  validates :region_id, numericality: { other_than: 1 } 
+  validates :shipped_day_id, numericality: { other_than: 1 } 
 end
