3f1e1f330bcc601a028d477b364dab64659bf61b
Mmy26 <yu.b1994masamura@gmail.com>
2021-10-27 16:03:08 +0900

    Robocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,8 +45,6 @@ class ItemsController < ApplicationController
 
   def contributor_confirmation
     @item = Item.find(params[:id])
-    unless current_user == @item.user
-      redirect_to root_path
-    end
+    redirect_to root_path unless current_user == @item.user
   end
 end
