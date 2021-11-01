d940ebb6dba8cab2927725084d0fb019f9676315
Mmy26 <yu.b1994masamura@gmail.com>
2021-11-01 17:32:09 +0900

    クレジットカード決済機能

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :zip_code, :region_id, :city, :street, :building, :phone_number, :user_id, :item_id
+  attr_accessor :zip_code, :region_id, :city, :street, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :zip_code
@@ -8,7 +8,7 @@ class PurchaseAddress
     validates :city
     validates :street
     validates :phone_number
-    #validates :token
+    validates :token
   end
   validates :zip_code,     format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
   validates :phone_number, numericality: { with: /\A[0-9]\z/,  message: 'is invalid. Input only number' }
