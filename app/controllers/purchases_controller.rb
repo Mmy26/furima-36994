79e72c31a80d577cf50c285ac825dc7fa7671490
Mmy26 <yu.b1994masamura@gmail.com>
2021-11-01 17:32:53 +0900

    ログイン状態でのリンクの制限

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,5 +1,6 @@
 class PurchasesController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!,       only: :index
+  before_action :contributor_confirmation, only: :index
   
   def index
     @purchase_address = PurchaseAddress.new
@@ -10,8 +11,8 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
-      
       redirect_to root_path
     else
       render :index
@@ -22,8 +23,23 @@ class PurchasesController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:zip_code, :region_id, :city, :street, :building,
-                                             :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+                                             :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
- 
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    binding.pry
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def contributor_confirmation
+    @item = Item.find(params[:item_id])
+    if current_user == @item.user
+      redirect_to root_path
+    end 
+  end
 end
